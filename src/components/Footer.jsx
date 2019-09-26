27c25ed409a479051c72abf17109c80fb67effac
StianIsmar <41960375+StianIsmar@users.noreply.github.com>
2019-09-26 21:47:07 +0200

    Fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,32 +17,42 @@ class Footer extends Component {
                     <Row>
                         <Col xs={12}><br></br></Col>
                     </Row>
+
                     <Row>
-                        <Col xs={2} class="d-flex justify-content-between">
+                        <Col id="icon"  xs={7}>
+                            <i class="fa fa-map-marker" aria-hidden="true"></i>
+                            <div class="address">Osloveien 33</div>
+                            <br></br>
+                            <i class="fa fa-phone" aria-hidden="true"></i>
+                            <div class = "address">42145161</div>
+                            <br></br>
+                            <i class="fa fa-envelope-o" aria-hidden="true"></i>
+                            <div class = "emailaddress">company@yourcompany.com</div>
+                            <div class="vl"></div>
+
+                        </Col>
+
+                        <Col xs={5} className="float-left">
                             <a href="https://www.instagram.com">
                                 <i id="instagramIcon" class="fa fa-instagram" aria-hidden="true"></i>
+                            </a>
+                            <a href="https://www.facebook.com">
+                                <i id="facebookIcon" class="icon fa fa-facebook" aria-hidden="true" ></i>
+                                <a href="https://www.twitter.com">
+                                    <i id="twitterIcon" class="icon fa fa-twitter" aria-hidden="true" ></i>
                                 </a>
+                            </a>
                         </Col>
-                        <Col xs={10}   className="float-left" >instagram.com/myCompany</Col>
                     </Row>
                     <Row>
-                        <Col xs={2}> <a href="https://www.facebook.com">
-                                <i id="facebookIcon" class="icon fa fa-facebook" aria-hidden="true" ></i>
-                            
-                            </a>
-                            </Col>
-                        <Col xs={10} className="float-left">facebook.com/myCompany</Col>
-                    </Row>
-                    <Row >
                         <Col xs={2}>
-                            <a href="https://www.twitter.com">
-                                <i id="twitterIcon" class="icon fa fa-twitter" aria-hidden="true" ></i>
-                            </a></Col>
-
-                        <Col xs={5} className="float-left">twitter.com/myCompany</Col>
-                        <Col xs={5}>Osloveien 40</Col>
+                        </Col>
+                        <Col xs={5} className="float-left"></Col>
 
                     </Row>
+
+
+
                     <Row>
                         <Col className="emptyRow" xs={12}><br></br></Col>
                     </Row>
